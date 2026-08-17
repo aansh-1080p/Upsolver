fa0d02d4cd6f1ca6d83453488b62d8e6766c2d0a
aansh-1080p <aanshofficiial@gmail.com>
2026-08-17 21:04:45 +0530

    chore: remove lenis smooth scrolling library and restore native scrolling

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'lenis/dist/lenis.css'
 import './index.css'
 import App from './App.jsx'
-import { ReactLenis } from 'lenis/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
-      <App />
-    </ReactLenis>
+    <App />
   </StrictMode>,
 )
 
